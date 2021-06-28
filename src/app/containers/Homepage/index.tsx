1c042714ba3446c1112fd5d420c21578202f311a
chi <chinedu.onyeoka@gmail.com>
2021-06-28 10:29:31 +0100

    adding nabar

--- src/app/containers/Homepage/index.tsx
+++ src/app/containers/Homepage/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components'
 import tw from 'twin.macro';
+import {Navbar} from '../../components/navbar'
 
 const PageContainer  = styled .div`
 ${tw`
@@ -15,6 +16,6 @@ ${tw`
 
 export function HomePage() {
   return <PageContainer>
-     Hello world
+     <Navbar />
     </PageContainer>
 }
