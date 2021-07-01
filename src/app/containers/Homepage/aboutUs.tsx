b8f22ad1621cee6424ceeb13e9332d478d04eef6
chi <chinedu.onyeoka@gmail.com>
2021-07-01 10:20:52 +0100

    booking-steps

---
+++ src/app/containers/Homepage/aboutUs.tsx
@@ -0,0 +1,92 @@
+
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+import { SCREENS } from "../../components/responsive";
+
+import JeepImg from "../../../assets/images/jeep.png";
+
+const AboutUsContainer = styled.div`
+  ${tw`
+    w-full
+    flex
+    flex-wrap
+    items-center
+    justify-center
+    pt-4
+    pb-4
+    pr-7
+    pl-7
+    md:pl-0
+    md:pr-0
+    bg-white
+  `};
+`;
+
+const CarContainer = styled.div`
+  width: auto;
+  height: 15em;
+  margin-left: -50px;
+  img {
+    width: auto;
+    height: 100%;
+  }
+  @media (min-width: ${SCREENS.md}) {
+    height: 28em;
+  }
+  @media (min-width: ${SCREENS.lg}) {
+    height: 30em;
+  }
+  @media (min-width: ${SCREENS["2xl"]}) {
+    height: 35em;
+    margin-left: 0;
+  }
+`;
+
+const InfoContainer = styled.div`
+  ${tw`
+    md:w-1/2
+    flex
+    flex-col
+    md:ml-6
+    2xl:ml-16
+  `};
+`;
+
+const Title = styled.h1`
+  ${tw`
+    text-black
+    text-2xl
+    md:text-5xl
+    font-extrabold
+    md:font-black
+    md:leading-normal
+  `};
+`;
+
+const InfoText = styled.p`
+  ${tw`
+    md:max-w-2xl
+    text-sm
+    md:text-base
+    text-gray-500
+    font-normal
+    mt-4
+  `};
+`;
+
+export function AboutUs() {
+  return (
+    <AboutUsContainer>
+      <CarContainer>
+        <img src={JeepImg} />
+      </CarContainer>
+      <InfoContainer>
+        <Title>Feel The Best Experience With Our Rental Deals</Title>
+        <InfoText>
+        Civil marine construction can be high risk. It’s vital you have the correct fleet and expertise to get the job done. Harmony Marine has industry experience spanning over 30 years, including 20 years hands-on experience in the Merchant navy, 5 years running a Marine services vessel fleet and 12 years as dedicated shipbroker. We’ve seen it all and we know firsthand what it takes to run a smooth operation. Whether you require a jack up for installing a wind farm or a fleet of support barges, we know exactly what is needed , and where to source it from.
+        </InfoText>
+      </InfoContainer>
+    </AboutUsContainer>
+  );
+}
